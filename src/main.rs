efb1ffc17055053121a75c659130cd64d6f77ff2
Andrew Stocker <sleepyams@gmail.com>
2024-03-01 10:54:25 -0700

    state machine test

--- src/main.rs
+++ src/main.rs
@@ -27,12 +27,12 @@ struct Action<S> {
 }
 
 
-type ActionSequenceIndex = usize;
+type SequenceIndex = usize;
 
 // Might want to add field for sequence length?
 #[derive(PartialEq, Eq, Debug)]
-struct ActionSequence<A> {
-    next: Option<ActionSequenceIndex>,
+struct Sequence<A> {
+    next: Option<SequenceIndex>,
     action: A
 }
 
@@ -55,57 +55,51 @@ fn main() {
     let sequences = vec![
 
         // Length 2 sequences of actions starting at state A
-        ActionSequence { next: Some(1),     action: &actionAtoA },  // 0
-        ActionSequence { next: None,        action: &actionAtoA },  // 1
+        Sequence { next: Some(1),     action: &actionAtoA },  // 0
+        Sequence { next: None,        action: &actionAtoA },  // 1
 
-        ActionSequence { next: Some(3),     action: &actionAtoA },  // 2
-        ActionSequence { next: None,        action: &actionAtoB },  // 3
+        Sequence { next: Some(3),     action: &actionAtoB },  // 2
+        Sequence { next: None,        action: &actionBtoA },  // 3
 
-        ActionSequence { next: Some(5),     action: &actionAtoB },  // 4
-        ActionSequence { next: None,        action: &actionBtoA },  // 5
+        Sequence { next: Some(5),     action: &actionAtoA },  // 4
+        Sequence { next: None,        action: &actionAtoB },  // 5
 
-        ActionSequence { next: Some(7),     action: &actionAtoB },  // 6
-        ActionSequence { next: None,        action: &actionBtoB },  // 7
+        Sequence { next: Some(7),     action: &actionAtoB },  // 6
+        Sequence { next: None,        action: &actionBtoB },  // 7
 
 
         // Length 2 sequences of actions starting at state B
-        ActionSequence { next: Some(9),     action: &actionBtoA },  // 8
-        ActionSequence { next: None,        action: &actionAtoA },  // 9
+        Sequence { next: Some(9),     action: &actionBtoA },  // 8
+        Sequence { next: None,        action: &actionAtoA },  // 9
 
-        ActionSequence { next: Some(11),    action: &actionBtoB },  // 10
-        ActionSequence { next: None,        action: &actionBtoA },  // 11
+        Sequence { next: Some(11),    action: &actionBtoB },  // 10
+        Sequence { next: None,        action: &actionBtoA },  // 11
 
-        ActionSequence { next: Some(13),    action: &actionBtoA },  // 12
-        ActionSequence { next: None,        action: &actionAtoB },  // 13
+        Sequence { next: Some(13),    action: &actionBtoA },  // 12
+        Sequence { next: None,        action: &actionAtoB },  // 13
 
-        ActionSequence { next: Some(15),    action: &actionBtoB },  // 14
-        ActionSequence { next: None,        action: &actionBtoB },  // 15
+        Sequence { next: Some(15),    action: &actionBtoB },  // 14
+        Sequence { next: None,        action: &actionBtoB },  // 15
 
 
         // Length 1 sequences of actions (i.e. just single actions)
-        ActionSequence { next: None,        action: &actionAtoA },  // 16
-        ActionSequence { next: None,        action: &actionAtoB },  // 17
-        ActionSequence { next: None,        action: &actionBtoA },  // 18
-        ActionSequence { next: None,        action: &actionBtoB },  // 19
+        Sequence { next: None,        action: &actionAtoA },  // 16
+        Sequence { next: None,        action: &actionAtoB },  // 17
+        Sequence { next: None,        action: &actionBtoA },  // 18
+        Sequence { next: None,        action: &actionBtoB },  // 19
 
-        ActionSequence { next: None,        action: &identity_action }  // 20
+        Sequence { next: None,        action: &identity_action }  // 20
     ];
 
 
-    //  -   The comonad structures on states A and B determine how sequences of actions fold.
-    //  -   This data also tells us how the actions correspond to state transitions.  The engine should infer from
-    //      the data of lenses what state an action can lead to.  For example, in this lens we declare that
-    //      AtoB then BtoB is a composable sequence of actions, and since the action BtoB is based at state B,
-    //      we can infer that AtoB points to state B.
-    //  -   Note that the above only makes sense if we have the structure of a comonad present in the program,
-    //      but any poly program should be built around a central comonad.
+    // The comonad structures on states A and B determine how sequences of actions fold together.
     let comonad_structure_for_stateA  = Lens {
         source: &stateA,
         target: &stateA,
         data: vec![
             Delegation { from: &sequences[0],   to: &sequences[16] },   // AtoA:AtoA -> AtoA
-            Delegation { from: &sequences[2],   to: &sequences[17] },   // AtoA:AtoB -> AtoB
-            Delegation { from: &sequences[4],   to: &sequences[16] },   // AtoB:BtoA -> AtoA
+            Delegation { from: &sequences[2],   to: &sequences[16] },   // AtoB:BtoA -> AtoA
+            Delegation { from: &sequences[4],   to: &sequences[17] },   // AtoA:AtoB -> AtoB
             Delegation { from: &sequences[6],   to: &sequences[17] },   // AtoB:BtoB -> AtoB
         ]
     };
@@ -138,4 +132,5 @@ fn main() {
             Delegation { from: &sequences[20],  to: &sequences[19] },
         ]
     };
+
 }
