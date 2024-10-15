303475c0fda2a97298b567c5c53cabc5011fecd4
Andrew Stocker <sleepyams@gmail.com>
2024-10-15 15:43:13 -0400

    wut

--- src/diagram2/mod.rs
+++ src/diagram2/mod.rs
@@ -6,95 +6,17 @@ pub use object::*;
 pub use action::*;
 
 
-#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
-pub struct Arrow<T> {
-    pub source: Object<T>,
-    pub target: Object<T>
+pub trait Arrow<T> {
+    fn source(&self) -> Object<T>;
+    fn target(&self) -> Object<T>;
 }
 
-impl<T> Sequence<Arrow<T>> {
-    pub fn source(&self) -> Object<T> {
-
+impl<T, A> Arrow<T> for Action<A> where A: Arrow<T> {
+    fn source(&self) -> Object<T> {
+        todo!()
     }
 
-    pub fn target(&self) -> Object<T> {
-        // actually want to fold..., but need "zero" and "unit" object/arrow...
-        self.0.iter().reduce(|x, y| x.0.source * y.0.source)
+    fn target(&self) -> Object<T> {
+        todo!()
     }
 }
-
-impl<T> Parallel<Arrow<T>> {
-    pub fn source(&self) -> Object<T> {
-        self.0.iter().reduce(|x, y| )
-    }
-
-    pub fn target(&self) -> Object<T> {
-        
-    }
-}
-
-
-pub struct Monad<T> {
-    object: Object<T>,
-    mult: Parallel<T>,
-    unit: Parallel<T>
-}
-
-pub struct Comonad<T> {
-    object: Object<T>,
-    comult: Parallel<T>,
-    counit: Parallel<T>
-}
-
-pub struct Engine<T> {
-    objects:  Vec<Object<T>>,
-    arrows:   Vec<Parallel<T>>,
-    monads:   Vec<Monad<T>>,
-    comonads: Vec<Comonad<T>>
-    
-    // For Monads, we want to actually keep track of arrows that defer to
-    // some power of the object of the monad.  If none of those exist, there is
-    // no point in expanding powers of a monadic object in the Query.
-}
-
-pub struct QueryResult<'engine, T> {
-    engine: &'engine Engine<T>,
-    existential: Parallel<T>,
-    stack: Vec<Parallel<T>>,
-    history: ()
-}
-
-impl<'engine, T> QueryResult<'engine, T> {
-    pub fn new(engine: &'engine Engine<T>, existential: Parallel<T>) -> QueryResult<T> {
-        QueryResult {
-            engine,
-            existential,
-            stack: Vec::new(),
-            history: ()
-        }
-    }
-}
-
-impl<'engine, T> Iterator for QueryResult<'engine, T> {
-    type Item = Parallel<T>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        // Implement a breadth-first search on Arrows, constructing new Arrows once a match has been found.
-        // Hash on Domains/Arrows that uniquely characterizes?:
-        //      https://stackoverflow.com/questions/1988665/hashing-a-tree-structure
-        //let source = self.existential.source;
-
-        // look for arrows whose target is this source.
-        for arrow in &self.engine.arrows {
-            
-        }
-
-        unimplemented!()
-    }
-}
-
-// impl Engine {
-//     pub fn query(&self, existential: Arrow) -> QueryResult {
-//         QueryResult::new(self, existential)
-//     }
-// }
--- src/diagram2/object.rs
+++ src/diagram2/object.rs
@@ -3,48 +3,149 @@ use std::ops::{Add, Mul};
 
 
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
-pub struct Atom<T>(T);
+pub enum Atom<T> {
+    Value(T),
+    Unit,
+    Zero
+}
+
 
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
-pub struct Product<T>(Vec<Atom<T>>);
+pub struct Product<T> {
+    pub data: Vec<Atom<T>>
+}
+
+impl<T> From<Atom<T>> for Product<T> {
+    fn from(atom: Atom<T>) -> Self {
+        Product {
+            data: [atom].into()
+        }
+    }
+}
+
 
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
-pub struct Object<T>(Vec<Product<T>>);
+pub struct Sum<T> {
+    pub data: Vec<Product<T>>
+}
 
-pub struct Zero;
+impl<T> From<Product<T>> for Sum<T> {
+    fn from(product: Product<T>) -> Self {
+        Sum {
+            data: [product].into()
+        }
+    }
+}
 
-pub struct Unit;
+impl<T> From<Atom<T>> for Sum<T> {
+    fn from(atom: Atom<T>) -> Self {
+        let product: Product<T> = atom.into();
+        product.into()
+    }
+}
 
+
+#[derive(Debug)]
+pub enum Object<T> {
+    Atom(Atom<T>),
+    Product(Product<T>),
+    Sum(Sum<T>)
+}
+
+impl<T> From<Atom<T>> for Object<T> {
+    fn from(atom: Atom<T>) -> Self {
+        Object::Atom(atom)
+    }
+}
+
+impl<T> From<Product<T>> for Object<T> {
+    fn from(product: Product<T>) -> Self {
+        Object::Product(product)
+    }
+}
+
+impl<T> From<Sum<T>> for Object<T> {
+    fn from(sum: Sum<T>) -> Self {
+        Object::Sum(sum)
+    }
+}
 
 impl<T: Ord> Object<T> {
+    pub fn unit() -> Object<T> {
+        Atom::Unit.into()
+    }
+
     pub fn zero() -> Object<T> {
-        Object(Vec::new())
+        Atom::Zero.into()
     }
 
     pub fn atom(t: T) -> Object<T> {
-        Self::product([Atom(t)])
+        Atom::Value(t).into()
     }
 
     pub fn product<I: IntoIterator<Item = Atom<T>>>(data: I) -> Object<T> {
         let mut data: Vec<Atom<T>> = data.into_iter().collect();
         data.sort();
-        Self::sum([Product(data)])
+        Product {
+            data
+        }.into()
     }
 
     pub fn sum<I: IntoIterator<Item = Product<T>>>(data: I) -> Object<T> {
         let mut data: Vec<Product<T>> = data.into_iter().collect();
         data.sort();
-        Object(data)
+        Sum {
+            data
+        }.into()
     }
 }
+
 
 impl<T> Add for Object<T> where T: Ord {
     type Output = Object<T>;
 
     fn add(self, rhs: Self) -> Self::Output {
-        Object::sum(self.0.into_iter().chain(rhs.0.into_iter()))
+        match (self, rhs) {
+            (Object::Atom(left), Object::Atom(right)) => match (left, right) {
+                (Atom::Zero, atom) | (atom, Atom::Zero) => {
+                    Object::Atom(atom)
+                },
+                (left, right) => {
+                    Object::sum([left.into(), right.into()])
+                }
+            },
+            (Object::Atom(atom), Object::Product(product)) |
+            (Object::Product(product), Object::Atom(atom)) => match atom {
+                Atom::Zero => product.into(),
+                atom => Object::sum([product, atom.into()])
+            },
+            (Object::Atom(atom), Object::Sum(sum)) |
+            (Object::Sum(sum), Object::Atom(atom)) => match atom {
+                Atom::Zero => sum.into(),
+                atom => {
+                    let mut data = sum.data;
+                    data.push(atom.into());
+                    Object::sum(data)
+                }
+            },
+            (Object::Product(product), Object::Sum(sum)) |
+            (Object::Sum(sum), Object::Product(product)) => {
+                let mut data = sum.data;
+                data.push(product);
+                Object::sum(data)
+            },
+            (Object::Product(left), Object::Product(right)) => {
+                Object::sum([left, right])
+            },
+            (Object::Sum(left), Object::Sum(mut right)) => {
+                let mut data = left.data;
+                data.append(&mut right.data);
+                Object::sum(data)
+            },
+        }
     }
 }
+
 
 impl<T> Mul for Object<T> where T: Ord + Clone {
     type Output = Object<T>;
