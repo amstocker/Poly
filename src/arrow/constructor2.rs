40e339f07ef5922f987f10692e8dce4e66939bfc
Andrew Stocker <sleepyams@gmail.com>
2024-10-24 10:02:30 -0400

    constructible arrows + display

--- src/arrow/constructor2.rs
+++ src/arrow/constructor2.rs
@@ -4,7 +4,7 @@ use im::{HashMap, Vector};
 
 
 
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+#[derive(Clone, PartialEq, Eq, Hash)]
 pub enum Constructor<T: Clone> {
     Atom(T),
     Sum(Vector<Constructor<T>>),
@@ -12,11 +12,36 @@ pub enum Constructor<T: Clone> {
     Sequence(Vector<Constructor<T>>)
 }
 
+impl<T: Clone> Constructor<T> {
+    pub fn sum(iter: impl IntoIterator<Item = Constructor<T>>) -> Constructor<T> {
+        Constructor::Sum(iter.into_iter().collect())
+    }
+
+    pub fn product(iter: impl IntoIterator<Item = Constructor<T>>) -> Constructor<T> {
+        Constructor::Product(iter.into_iter().collect())
+    }
+
+    pub fn sequence(iter: impl IntoIterator<Item = Constructor<T>>) -> Constructor<T> {
+        Constructor::Sequence(iter.into_iter().collect())
+    }
+}
+
+impl<T: Clone + std::fmt::Display> std::fmt::Display for Constructor<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Constructor::Atom(atom) => write!(f, "{}", atom),
+            Constructor::Sum(elems)
+                => write!(f, "{}", elems.iter().map(|elem| elem.to_string()).collect::<Vec<_>>().join(" + ")),
+            Constructor::Product(elems)
+                => write!(f, "({})", elems.iter().map(|elem| elem.to_string()).collect::<Vec<_>>().join(", ")),
+            Constructor::Sequence(elems)
+                => write!(f, "[{}]", elems.iter().map(|elem| elem.to_string()).collect::<Vec<_>>().join(" -> ")),
+        }
+    }
+}
+
 pub trait Constructible<T> {
     fn atom(t: T) -> Self;
-    fn zero() -> Self;
-    fn unit() -> Self;
-    fn identity() -> Self;
     fn add(self, other: Self) -> Self;
     fn mult(self, other: Self) -> Self;
     fn compose(self, other: Self) -> Self;
@@ -27,20 +52,26 @@ impl<T: Clone> Constructor<T> {
         match self {
             Constructor::Atom(t) => Constructible::atom(t),
             Constructor::Sum(elems) => elems.into_iter()
-                .fold(Constructible::zero(), |acc, other| acc.add(other.build())),
+                .map(|elem| elem.build())
+                .reduce(|acc: A, elem| acc.add(elem)).unwrap(),
             Constructor::Product(elems) => elems.into_iter()
-                .fold(Constructible::unit(), |acc, other| acc.mult(other.build())),
+                .map(|elem| elem.build())
+                .reduce(|acc: A, elem| acc.add(elem)).unwrap(),
             Constructor::Sequence(elems) => elems.into_iter()
-                .fold(Constructible::identity(), |acc, other| acc.compose(other.build())),
+                .map(|elem| elem.build())
+                .reduce(|acc: A, elem| acc.add(elem)).unwrap()
         }
     }
 }
 
 
-pub enum Arrow<T: Clone> {
-    Zero,
-    Identity,
-    Arrow(HashMap<Constructor<T>, Constructor<T>>)
+#[derive(Clone)]
+pub struct Arrow<T: Clone + Eq + Hash>(HashMap<Constructor<T>, Constructor<T>>);
+
+impl<T: Clone + Eq + Hash> Arrow<T> {
+    pub fn arrow(iter: impl IntoIterator<Item = (Constructor<T>, Constructor<T>)>) -> Arrow<T> {
+        Arrow(iter.into_iter().collect())
+    }
 }
 
 impl<T: Clone + Eq + Hash> Constructible<Arrow<T>> for Arrow<T> {
@@ -48,49 +79,53 @@ impl<T: Clone + Eq + Hash> Constructible<Arrow<T>> for Arrow<T> {
         arrow
     }
 
-    fn zero() -> Self {
-        Arrow::Zero
-    }
-
-    fn unit() -> Self {
-        Arrow::Identity
-    }
-
-    fn identity() -> Self {
-        Arrow::Identity
-    }
-
     fn add(self, other: Self) -> Self {
-        match (self, other) {
-            (Arrow::Arrow(left), Arrow::Arrow(right)) => todo!(),
-            (Arrow::Identity, arrow) | (arrow, Arrow::Identity) => arrow,
-            (Arrow::Zero, _) | (_, Arrow::Zero) => Arrow::Zero
+        let (Arrow(left), Arrow(right)) = (self, other);
+        let mut sum = left.clone();
+        for (x, y) in right.iter() {
+            sum.entry(x.clone())
+               .and_modify(|y2| *y2 = Constructor::sum([y.clone(), y2.clone()]))
+               .or_insert(y.clone());
         }
+        Arrow(sum)
     }
 
     fn mult(self, other: Self) -> Self {
-        match (self, other) {
-            (Arrow::Arrow(left), Arrow::Arrow(right)) => todo!(),
-            (Arrow::Identity, arrow) | (arrow, Arrow::Identity) => arrow,
-            (Arrow::Zero, _) | (_, Arrow::Zero) => Arrow::Zero
+        let (Arrow(left), Arrow(right)) = (self, other);
+        let mut product = HashMap::new();
+        for (x1, y1) in left.iter() {
+            for (x2, y2) in right.iter() {
+                product.insert(
+                    Constructor::product([x1.clone(), x2.clone()]),
+                    Constructor::product([y1.clone(), y2.clone()])
+                );
+            }
         }
+        Arrow(product)
     }
 
     fn compose(self, other: Self) -> Self {
-        match (self, other) {
-            (Arrow::Arrow(first), Arrow::Arrow(second)) => {
-                let mut composition = HashMap::new();
-                for (x, y1) in first.iter() {
-                    for (y2, z) in second.iter() {
-                        if y1 == y2 {
-                            composition.insert(x.clone(), z.clone());
-                        }
-                    }
+        let (Arrow(first), Arrow(second)) = (self, other);
+        let mut composition = first.clone();
+        'outer: for (x, y1) in first.iter() {
+            for (y2, z) in second.iter() {
+                if y1 == y2 {
+                    composition.insert(x.clone(), z.clone());
+                    continue 'outer;
                 }
-                Arrow::Arrow(composition)
-            },
-            (Arrow::Identity, arrow) | (arrow, Arrow::Identity) => arrow,
-            (Arrow::Zero, _) | (_, Arrow::Zero) => Arrow::Zero
+            }
+            composition.remove(x);
         }
+        Arrow(composition)
+    }
+}
+
+impl<T: Clone + Eq + Hash + std::fmt::Display> std::fmt::Display for Arrow<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let pairs = self.0.iter()
+            .map(|(x, y)| format!("{} => {}", x.to_string(), y.to_string()))
+            .collect::<Vec<_>>()
+            .join(" , ");
+        write!(f, "{{ {} }}", pairs)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,61 +4,29 @@ mod arrow;
 
 
 
-use arrow::*;
-
-#[derive(Clone, PartialEq, Eq, PartialOrd, Ord)]
-struct Test {
-    source: Object<char>,
-    target: Object<char>
-}
-
-impl std::fmt::Display for Test {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "[{} -> {}]", self.source, self.target)
-    }
-}
-
-impl Arrow<char> for Test {
-    fn source(&self) -> Object<char> {
-        self.source.clone()
-    }
-
-    fn target(&self) -> Object<char> {
-        self.target.clone()
-    }
-}
-
+use arrow::constructor2::*;
 
 
 
 
 fn main() {
 
-    let x: Object<char> = Atom::Value('X').into();
-    let y: Object<char> = Atom::Value('Y').into();
-    let z: Object<char> = Atom::Value('Z').into();
 
-    println!("{}", x.clone() * y.clone() * x.clone());
-    println!("{}", y.clone() + x.clone() + y.clone() * z.clone());
-    println!("{}", (z.clone() + y.clone()) * x.clone());
-    println!("{}", x.clone() * (z.clone() + y.clone()));
-    println!("{}", Object::unit() * x.clone());
-    println!("{}", x.clone() * Object::zero());
+    let x = Constructor::Atom('x');
+    let y = Constructor::Atom('y');
+    let z = Constructor::Atom('z');
 
+    let a = Constructor::Atom('a');
+    let b = Constructor::Atom('b');
 
-    let f: Action<Test> = Operation::Value(Test { source: x.clone(), target: y.clone() }).into();
-    let g: Action<Test> = Operation::Value(Test { source: y.clone(), target: z.clone() }).into();
-    let h: Action<Test> = Operation::Value(Test { source: z.clone(), target: x.clone() }).into();
+    let f = Arrow::arrow([(x.clone(), a.clone()), (y.clone(), a.clone()), (z.clone(), b.clone())]);
+    let g = Arrow::arrow([(b.clone(), a.clone()), (a.clone(), b.clone())]);
+    let h = Arrow::arrow([(x.clone(), b.clone()), (a.clone(), a.clone())]);
 
-    let f1 = h.clone() + f.clone();
-    let f2 = f.clone() * g.clone();
-    let f3 = g.clone() * (f.clone() + h.clone());
-    let f4 = g.clone() * f.clone();
-
-    println!("{} : {} -> {}", f1, f1.source(), f1.target());
-    println!("{} : {} -> {}", f2, f2.source(), f2.target());
-    println!("{} : {} -> {}", f3, f3.source(), f3.target());
-    println!("{} : {} -> {}", f4, f4.source(), f4.target());
-
-
+    println!("f: {f}");
+    println!("g: {g}");
+    println!("h: {h}");
+    println!("f -> g: {}", f.clone().compose(g.clone()));
+    println!("f -> h: {}", f.clone().compose(h.clone()));
+    println!("f + g: {}", f.add(h));
 }
